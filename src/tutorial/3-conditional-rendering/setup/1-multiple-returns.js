f13105644c089262f62a112189737dd79979e5a7
Jenner Garcia <jenner.garcia@gmail.com>
2021-05-27 11:34:08 -0400

    useEffect

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -8,8 +8,17 @@ const MultipleReturns = () => {
 
   useEffect(() => {
     fetch(url)
-      .then((resp) => resp.json())
+      .then((resp) => {
+        if (resp.status >= 200 && resp.status <= 299) {
+          return resp.json();
+        } else {
+          setIsLoading(false);
+          setIsError(true);
+          throw new Error(resp.statusText);
+        }
+      })
       .then((user) => {
+        //console.log(user);
         const { login } = user;
         setUser(login);
         setIsLoading(false);
